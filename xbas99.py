ce99b70d578a4c60f5176525564e8e7e19a85b11
Christopher KOBAYASHI <chris@disavowed.jp>
2016-04-05 15:18:29 +0900

    Fix off-by-one and remove debug print statement.

--- xbas99.py
+++ xbas99.py
@@ -196,8 +196,7 @@ class BasicProgram:
             # convert long format INT/VAR 254 to PROGRAM
 	    if _tifiles:
 		newdata = data[0:11]
-		print len(data)
-		for i in range(1, (len(data) / 255)):
+		for i in range(1, (len(data) / 256)):
 		   newdata += data[(i*256):(i*256)+255]
 		data = newdata
             program, p = "", 11
