e8c3ee4127123b78207fa012bec586898fd7c415
Christopher Kobayashi <wileyc@elan.disavowed.jp>
2016-04-06 15:32:29 +0900

    Revert "Convert from IV254 to PROGRAM to handle TIFILES formatted files.  This should fix erroneous "program is corrupt" issues while decoding."
    This reverts commit fac201de97e56184417ebf460f39b5f3191de1d6.

--- xbas99.py
+++ xbas99.py
@@ -22,7 +22,6 @@
 import sys
 import re
 import os.path
-import traceback
 
 
 VERSION = "1.5.0"
@@ -170,16 +169,15 @@ class BasicProgram:
     # maximum number of bytes/tokens per BASIC line
     maxTokensPerLine = 254
     
-    def __init__(self, data=None, source=None, long_=False, _tifiles=False):
+    def __init__(self, data=None, source=None, long_=False):
         self.lines = {}
         self.textlits = []
         self.warnings = []
         if data:
             try:
-                self.load(data, long_, _tifiles)
+                self.load(data, long_)
             except IndexError:
                 self.warn("Program file is corrupted")
-		print traceback.format_exc()
         elif source:
             self.parse(source)
 
@@ -190,13 +188,7 @@ class BasicProgram:
 
     # program -> source
 
-    def load(self, data, long_, _tifiles):
-	if _tifiles:
-		newdata = data[0:11]
-		print len(data)
-		for i in range(1, (len(data) / 255)):
-		   newdata += data[(i*256):(i*256)+255]
-		data = newdata
+    def load(self, data, long_):
         """load tokenized BASIC program"""
         if long_ or data[1:3] == "\xab\xcd":
             # convert long format INT/VAR 254 to PROGRAM
@@ -476,7 +468,6 @@ class BasicProgram:
 
 def main():
     import argparse
-    tifiles = False
 
     args = argparse.ArgumentParser(
         version=VERSION,
@@ -520,12 +511,11 @@ def main():
                     image = fin.read()
 	    if opts.astifiles:
 		    image = image[128:]
-		    tifiles = True
             if opts.merge:
                 program = BasicProgram()
                 program.merge(image)
             else:
-                program = BasicProgram(data=image, long_=opts.long_, _tifiles=tifiles)
+                program = BasicProgram(data=image, long_=opts.long_)
             data = program.getSource()
             output = "-" if opts.list_ else opts.output or barename + ".b99"
         elif opts.dump:
